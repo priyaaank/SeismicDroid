ba67f453c9ccb18ab9af026b77aae14fa3d9c163
Priyank Gupta <priyaaank@gmail.com>
2010-05-24 10:17:18 +0530

    Added prefrences page. It renders from an xml currently. Prefrences is not accessible from anywhere right now.

--- gen/com/barefoot/pocketshake/R.java
+++ gen/com/barefoot/pocketshake/R.java
@@ -15,15 +15,15 @@ public final class R {
         public static final int quake=0x7f020001;
     }
     public static final class id {
-        public static final int date_of_occurance=0x7f050008;
-        public static final int intensity_value=0x7f050005;
-        public static final int linear_layout_inner=0x7f050006;
-        public static final int linear_layout_outer=0x7f050003;
-        public static final int location_value=0x7f050007;
-        public static final int mainlayout=0x7f050000;
-        public static final int mapview=0x7f050001;
-        public static final int relative_layout_inner=0x7f050004;
-        public static final int zoomview=0x7f050002;
+        public static final int date_of_occurance=0x7f060008;
+        public static final int intensity_value=0x7f060005;
+        public static final int linear_layout_inner=0x7f060006;
+        public static final int linear_layout_outer=0x7f060003;
+        public static final int location_value=0x7f060007;
+        public static final int mainlayout=0x7f060000;
+        public static final int mapview=0x7f060001;
+        public static final int relative_layout_inner=0x7f060004;
+        public static final int zoomview=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -31,10 +31,14 @@ public final class R {
         public static final int quake=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int create_db_sql=0x7f040003;
-        public static final int feed_url=0x7f040000;
-        public static final int map_activity=0x7f040002;
-        public static final int upgrade_db_sql=0x7f040004;
+        public static final int app_name=0x7f050001;
+        public static final int create_db_sql=0x7f050004;
+        public static final int feed_url=0x7f050000;
+        public static final int map_activity=0x7f050003;
+        public static final int pref_activity=0x7f050002;
+        public static final int upgrade_db_sql=0x7f050005;
+    }
+    public static final class xml {
+        public static final int pref=0x7f040000;
     }
 }
---
+++ src/com/barefoot/pocketshake/QuakePrefrences.java
@@ -0,0 +1,12 @@
+package com.barefoot.pocketshake;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class QuakePrefrences extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref);
+    }
+}
